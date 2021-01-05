9c36fc87c2d1275298a0ac372819915c1461f650
M.Hamed <mohammadhamed1380@gmail.com>
2021-01-05 22:17:06 +0330

    'update'

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,21 @@ from extensions.utils import jalali_converter
 
 # Create your models here.
 
+class Category(models.Model):
+    title = models.CharField(max_length=300, verbose_name="عنوان دسته‌بندی")
+    slug = models.SlugField(max_length=100, unique=True, verbose_name="آدرس دسته‌بندی")
+    status = models.BooleanField(default=True, verbose_name="آیا نمایش داده شود؟")
+    position = models.IntegerField(verbose_name="پوزیشن")
+
+    class Meta:
+        verbose_name = "دسته‌بندی"
+        verbose_name_plural = "دسته‌بندی‌ها"
+        ordering = ['position']
+
+    def __str__(self):
+        return self.title
+
+
 class Article(models.Model):
     STATUS_CHOICES = (
         ('d', 'پیش نویس'),
@@ -12,6 +27,7 @@ class Article(models.Model):
     )
     title = models.CharField(max_length=300, verbose_name="عنوان مقاله")
     slug = models.SlugField(max_length=100, unique=True, verbose_name="آدرس مقاله")
+    category = models.ManyToManyField(Category, verbose_name="دسته‌بندی" )
     description = models.TextField()
     thumbail = models.ImageField(upload_to="images", verbose_name="تصویر مقاله")
     publish = models.DateTimeField(default=timezone.now, verbose_name="زمان انتشار")
@@ -22,10 +38,14 @@ class Article(models.Model):
     class Meta:
         verbose_name = "مقاله"
         verbose_name_plural = "مقالات"
+        ordering = ['-publish']
+
+
 
     def __str__(self):
         return self.title
 
     def jpublish(self):
         return jalali_converter(self.publish)
+
     jpublish.short_description = "زمان انتشار"
--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,20 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 # from django.http import HttpResponse, JsonResponse, Http404
-from .models import Article
+from .models import Article, Category
+
+
 # Create your views here.
 def home(request):
     context = {
-        "articles": Article.objects.filter(status='p').order_by('-publish')
+        "articles": Article.objects.filter(status='p'),
+        "category": Category.objects.filter(status=True)
     }
     return render(request, "blog/home.html", context)
 
+
 def detail(request, slug):
     context = {
-        "article": get_object_or_404(Article, slug=slug, status='p')
+        "article": get_object_or_404(Article, slug=slug, status='p'),
+        "category": Category.objects.filter(status=True)
     }
     return render(request, "blog/detail.html", context)
-
