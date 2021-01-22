096994ddabe8871dddafc9cb570b316278dcbe12
M.Hamed <mohammadhamed1380@gmail.com>
2021-01-23 03:09:11 +0330

    'update'

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from .models import Article, Category
 # Create your views here.
 def home(request, page=1):
     articles_list = Article.objects.published()
-    paginator = Paginator(articles_list, 5)
+    paginator = Paginator(articles_list, 2)
     articles = paginator.get_page(page)
     context = {
         "articles": articles,
@@ -24,8 +24,8 @@ def detail(request, slug):
 
 def category(request, slug, page=1):
     category = get_object_or_404(Category, slug=slug, status=True)
-    articles_list = category.articles.published
-    paginator = Paginator(articles_list, 5)
+    articles_list = category.articles.published()
+    paginator = Paginator(articles_list, 2)
     articles = paginator.get_page(page)
     context = {
         "category": category,
